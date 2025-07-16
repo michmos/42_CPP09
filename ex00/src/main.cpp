d32e66b2ed7783d80fafe3399d7a332de5cbef91
Michael Moser <Michael.Moser11@web.de>
2025-07-16 19:51:58 +0200

    ex00: update main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,32 +1,70 @@
 #include "../inc/Date.hpp"
+#include "../inc/BtcConverter.hpp"
+#include <cstring>
+#include <fstream>
 #include <iostream>
+#include <regex>
 
-int main(void) {
-	while (true) {
-		std::string input;
-		std::cout << "Date: ";
-		std::cin >> input;
-		if (input == "exit") {
-			break;
+#define CSV_PATH "data.csv"
+
+
+void printInputError(size_t lineNbr, const std::string& str) {
+	std::cerr << "Error in line: " << lineNbr << ": " << str << std::endl;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		std::cerr <<"Usage: ./btc <pathToInputFile>" << std::endl;
+		return 1;
+	}
+	
+	try {
+		BtcConverter converter(CSV_PATH);
+
+		std::ifstream	inputFile(argv[1]);
+		if (inputFile.fail()) {
+			std::cerr << "Could not open input file: " << strerror(errno) << std::endl;
+			return 1;
 		}
-		try {
-			Date date(input);
-			while (true) {
-				std::cout << "Advance date? (y/n): ";
-				std::cin >> input;
-				if (input == "y") {
-					date.advanceDate();
-					std::cout << "New date: " << date << std::endl;
-				} else {
-					break;
+
+		std::string line;
+		for (size_t lineNbr = 0; std::getline(inputFile, line); ++lineNbr) {
+			if (line == "date | value" && lineNbr == 0) { continue; }
+
+			std::smatch match;
+			if (std::regex_match(line, match, std::regex("(" REGEX_DATE ") \\| (" REGEX_NUMBER ")"))) {
+				try {
+					Date date(match[1]);
+					float btcAmount = std::stof(match[2]);
+					if (btcAmount < 0) {
+						printInputError(lineNbr, "negative value");
+						continue;
+					} else if (btcAmount > 1000) {
+						printInputError(lineNbr, "value > 1000");
+						continue;
+					}
+					std::optional<float> convertedValue = converter.getValue(date.getDate(), btcAmount);
+					if (!convertedValue.has_value()) {
+						printInputError(lineNbr, "date past last database date");
+						continue;
+					}
+					std::cout << match[1] << " => " << btcAmount << " => " << convertedValue.value() << std::endl;
+				} catch (std::exception &e) {
+					printInputError(lineNbr, e.what());
 				}
+		
+			} else {
+				printInputError(lineNbr, "invalid input");
 			}
-		} catch (const std::invalid_argument& e) {
-			std::cout << "Invalid date format: " << e.what() << std::endl;
+		
 		}
-
-
+	} catch (std::exception& e)  {
+		std::cerr << e.what() << std::endl;
+		return (1);
 	}
 
 
 }
+
+// TODO:open csv file and parse csv file into a map
+// TODO:check validity of input line
