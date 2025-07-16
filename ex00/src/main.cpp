7c8ddb3f219e96b9766a09171440bccbb89548ee
Michael Moser <Michael.Moser11@web.de>
2025-07-16 20:25:51 +0200

    ex00: simplify main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -4,12 +4,35 @@
 #include <fstream>
 #include <iostream>
 #include <regex>
+#include <stdexcept>
 
 #define CSV_PATH "data.csv"
 
-
 void printInputError(size_t lineNbr, const std::string& str) {
 	std::cerr << "Error in line: " << lineNbr << ": " << str << std::endl;
+}
+
+float getValue(const std::string& validationDate, const std::string& amount) {
+	static BtcConverter converter(CSV_PATH);
+
+	// create Date object as validity check
+	Date date(validationDate);
+
+	// convert btcAmount
+	float btcAmount = std::stof(amount);
+	if (btcAmount < 0) {
+		throw std::invalid_argument("negative value");
+	} 
+	if (btcAmount > 1000) {
+		throw std::invalid_argument("value > 1000");
+	}
+
+	// get total value
+	auto convertedValue = converter.getValue(date.getDate(), btcAmount);
+	if (!convertedValue) {
+		throw std::invalid_argument("date before first database entry");
+	}
+	return (convertedValue.value());
 }
 
 int main(int argc, char* argv[]) {
@@ -19,8 +42,7 @@ int main(int argc, char* argv[]) {
 	}
 	
 	try {
-		BtcConverter converter(CSV_PATH);
-
+		// open input file
 		std::ifstream	inputFile(argv[1]);
 		if (inputFile.fail()) {
 			std::cerr << "Could not open input file: " << strerror(errno) << std::endl;
@@ -31,32 +53,20 @@ int main(int argc, char* argv[]) {
 		for (size_t lineNbr = 0; std::getline(inputFile, line); ++lineNbr) {
 			if (line == "date | value" && lineNbr == 0) { continue; }
 
+			// retrieve date and btcAmount and check syntax
 			std::smatch match;
-			if (std::regex_match(line, match, std::regex("(" REGEX_DATE ") \\| (" REGEX_NUMBER ")"))) {
-				try {
-					Date date(match[1]);
-					float btcAmount = std::stof(match[2]);
-					if (btcAmount < 0) {
-						printInputError(lineNbr, "negative value");
-						continue;
-					} else if (btcAmount > 1000) {
-						printInputError(lineNbr, "value > 1000");
-						continue;
-					}
-					std::optional<float> convertedValue = converter.getValue(date.getDate(), btcAmount);
-					if (!convertedValue.has_value()) {
-						printInputError(lineNbr, "date past last database date");
-						continue;
-					}
-					std::cout << match[1] << " => " << btcAmount << " => " << convertedValue.value() << std::endl;
-				} catch (std::exception &e) {
-					printInputError(lineNbr, e.what());
-				}
-		
-			} else {
+			if (!std::regex_match(line, match, std::regex("(" REGEX_DATE ") \\| (" REGEX_NUMBER ")"))) {
 				printInputError(lineNbr, "invalid input");
+				continue;
 			}
-		
+
+			// get total value
+			try {
+				float value = getValue(match[1], match[2]);
+				std::cout << match[1] << " => " << match[2] << " => " << value << std::endl;
+			} catch (std::exception &e) {
+				printInputError(lineNbr, e.what());
+			}
 		}
 	} catch (std::exception& e)  {
 		std::cerr << e.what() << std::endl;
@@ -65,6 +75,3 @@ int main(int argc, char* argv[]) {
 
 
 }
-
-// TODO:open csv file and parse csv file into a map
-// TODO:check validity of input line
