3ef96110cd5ff2fb22711b876878978e6cb3e33d
Michael Moser <Michael.Moser11@web.de>
2025-07-27 16:23:09 +0200

    ex02: add more tests

--- ex02/src/tests/PmergeMe_test.cpp
+++ ex02/src/tests/PmergeMe_test.cpp
@@ -1,4 +1,5 @@
 
+#include <climits>
 #include <gtest/gtest.h>
 #include "../../inc/PmergeMe.hpp"
 
@@ -26,3 +27,32 @@ TEST(PmergeMe, evenNumElements_unique) {
 		  19,12, 5,4, 20, 13, 7};
 	testSort(vec);
 }
+
+TEST(PmergeMe, oddNumElements_unique) {
+	std::vector<int> vec = {5, 1, 3};
+	testSort(vec);
+	vec = {100, -50, 0, 25, -75};
+	testSort(vec);
+	vec = {7, 2, 9, 4, 1, 8, 3};
+	testSort(vec);
+}
+
+TEST(PmergeMe, edgeValues_unique) {
+	std::vector<int> vec = {INT_MIN, 0, INT_MAX};
+	testSort(vec);
+	vec = {INT_MAX, INT_MIN, 1, -1, 0};
+	testSort(vec);
+	vec = {INT_MIN, INT_MAX, -100, 100, 0, 50, -50};
+	testSort(vec);
+}
+
+TEST(PmergeMe, withDuplicates) {
+	std::vector<int> vec = {2, 2, 1, 3, 3, 1};
+	testSort(vec);
+	vec = {5, 5, 5, 5, 5};
+	testSort(vec);
+	vec = {INT_MIN, INT_MIN, INT_MAX, INT_MAX, 0, 0};
+	testSort(vec);
+}
+
+
